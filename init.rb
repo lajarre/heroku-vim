39ba2fe70e6af1a52c20afe1081f67069e715cd4
lajarre <alexandre.hajjar@gmail.com>
2014-03-19 16:26:53 +0100

    init.rb: added `export EDITOR=vim` for ipython

--- init.rb
+++ init.rb
@@ -19,6 +19,7 @@ class Heroku::Command::Vim < Heroku::Command::Run
 mkdir vim
 curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
 export PATH=$PATH:/app/vim/bin
+export EDITOR=vim
 bash
     CMD
   end
