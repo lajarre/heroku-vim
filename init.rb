9fe32010f1457e8a1fac181a5f3881afa53868d7
lajarre <alexandre.hajjar@gmail.com>
2014-03-20 15:36:22 +0100

    set -o vi

--- init.rb
+++ init.rb
@@ -21,6 +21,7 @@ mkdir vim
 curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
 export PATH=$PATH:/app/vim/bin
 export EDITOR=vim
+set -o vi
 #{inputrc}
 bash
     CMD
